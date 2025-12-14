fea43fd88443d9e64454f4d95924b8284ac4fca4
subahan islam <subahanislam523@gmail.com>
2025-12-14 19:24:46 +0600

    adde to confing file and env setup project database

---
+++ src/app/config/env.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+interface EnvConfig {
+  PORT: string;
+  DATABASE_URL: string;
+  NODE_ENV: "development" | "production" | "test";
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariables = ["PORT", "DATABASE_URL", "NODE_ENV"];
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing required environment variable ${key}`);
+    }
+  })
+  return {
+    PORT: process.env.PORT as string,
+    DATABASE_URL: process.env.DATABASE_URL as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production" | "test",
+  };
+};
+export const envVars = loadEnvVariables();
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,19 @@
 /* eslint-disable no-console */
 import { Server } from "http";
-
+import { envVars } from "./app/config/env.js";
 import mongoose from "mongoose";
 import app from "./app.js";
-
-
 
 let server: Server;
 
 const startServer = async () => {
   // await mongoose.connect(process.env.MONGO_URI || "", {})
   try {
-    await mongoose.connect(
-      "mongodb+srv://PhTourManagement:vwJ290BHUqNMkKAw@cluster0.yqmtelq.mongodb.net/tour-management-backend?appName=Cluster0"
-    );
+    console.log(envVars.NODE_ENV);
+    await mongoose.connect(envVars.DATABASE_URL);
     console.log("Connected to DB");
-    server = app.listen(8000, () => {
-      console.log("Server is running port 8000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is running port ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
