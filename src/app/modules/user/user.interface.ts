3273a87e43713ed711e42188dcda4a08e825ed58
subahan islam <subahanislam523@gmail.com>
2025-12-15 16:56:50 +0600

    added to a user interface create

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,40 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  USER = "USER",
+  ADMIN = "ADMIN",
+  GUIDE = "GUIDE",
+}
+
+// auth providers
+// email  password
+// google password authentication
+
+export interface IAuthProvider {
+  provider: "google" | "credentials"; // "Google" , "credential"
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",  
+}
+
+export interface IUser {
+  _id?: Types.ObjectId;
+  name: string;
+  email: string;
+  password?: string;
+  role: Role;
+  phone ?: string;
+  picture ?: string;
+  address ?: string;
+  isDeleted?: boolean;
+  isActive?: IsActive;
+  isVerified?: boolean;
+  auths: IAuthProvider[];
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
